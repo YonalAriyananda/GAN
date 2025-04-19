19caa8814fcb3e55d636293340b35be7faba5319
YonalAriyananda <yonalariyananda@gmail.com>
2025-04-19 21:16:48 +0100

    Added pytorch dependencies, plus basic class and function structure

---
+++ GAN.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+
+
+
+class Generator(nn.module):
+
+
+
+class Discriminator(nn.module):
+
+
+
+class Trainer():
+    
+def DataLoader():
+    return x
+
+def main():
+    return x
