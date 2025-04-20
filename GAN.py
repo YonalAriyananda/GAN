e574e4898ac26f8fbc154cf1d9d27ca02bfc19ad
YonalAriyananda <yonalariyananda@gmail.com>
2025-04-21 00:56:04 +0100

    Added Data class and all it's required variables, methods and immediate functionality

--- GAN.py
+++ GAN.py
@@ -1,7 +1,10 @@
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
 
 
 
@@ -14,9 +17,47 @@ class Discriminator(nn.module):
 
 
 class Trainer():
+
+
+class Data():         
+
+
+    def __init__(self,directory,image_size,batch_size,num_of_workers):
+        self.image_size = image_size  
+        self.batch_size = batch_size
+        self.directory  = directory
+        self.num_of_workers = num_of_workers 
+
+        self.transform = self.transform_data()
+        self.dataset = self.load_data()
+        self.dataloader = self.make_dataloader()
+
+
+    def transform_data(self):
+        return transforms.Compose([
+            transforms.Resize(self.image_size),
+            transforms.CenterCrop(self.image_size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    def load_data(self):
+        return datasets.ImageFolder(root = self.directory, transform = self.transform)
+        
+
+    def make_dataloader(self):
+        return DataLoader(
+            self.dataset,
+            batch_size = self.batch_size,
+            shuffle=True,
+            num_workers = self.num_of_workers
+        )
+
+    def get_dataloader(self):
+        return self.dataloader
+
+
     
-def DataLoader():
-    return x
 
 def main():
     return x
